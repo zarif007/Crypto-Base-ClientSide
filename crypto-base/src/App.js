973064f697eae5d0f017a7874a0156ae6152f3f6
zarif007 <zarifhuq786@gmail.com>
2021-12-17 17:31:13 +0600

    Added navbar

--- crypto-base/src/App.js
+++ crypto-base/src/App.js
@@ -1,5 +1,10 @@
 import { Layout } from 'antd';
-import { Route, Switch } from 'react-router';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import { NavBar, HomePage, Exchanges, CryptoCurrencies, CryptoDetails, News } from './Components'
 import Navbar from './Components/NavBar/NavBar';
@@ -7,26 +12,28 @@ import Navbar from './Components/NavBar/NavBar';
 function App() {
   return (
     <div>
-      <Navbar/>
-      <div className="routes container">
-        <Switch>
-          <Route exact path='/'>
-            <HomePage/>
-          </Route>
-          <Route exact path='/exchanges'>
-            <Exchanges/>
-          </Route>
-          <Route exact path='/cryptocurrencies'>
-            <CryptoCurrencies/>
-          </Route>
-          <Route exact path='/crypto/:coinId'>
-            <CryptoDetails/>
-          </Route>
-          <Route exact path='/news'>
-            <News /> 
-          </Route>
-        </Switch>
-      </div>
+      <Router>
+        <Navbar/>
+        <div className="container">
+          <Switch>
+            <Route exact path='/'>
+              <HomePage/>
+            </Route>
+            <Route exact path='/exchanges'>
+              <Exchanges/>
+            </Route>
+            <Route exact path='/cryptocurrencies'>
+              <CryptoCurrencies/>
+            </Route>
+            <Route exact path='/crypto/:coinId'>
+              <CryptoDetails/>
+            </Route>
+            <Route exact path='/news'>
+              <News /> 
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- crypto-base/src/Components/NavBar/NavBar.js
+++ crypto-base/src/Components/NavBar/NavBar.js
@@ -11,37 +11,22 @@ import {
 
 import icon from '../../Images/icon.png'
 import { Card } from 'antd';
+import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
 
     return (
         <>
-          <Nav>
-            <NavLink to='/'>
-              <img style={{height: 50}} src={icon} alt='logo' />
-            </NavLink>
-            <Bars />
-            <NavMenu>
-              <NavLink to='/cryptocurrencies' activeStyle>
-                Crypto Currencies
-              </NavLink>
-              <NavLink to='/services' activeStyle>
-                Services
-              </NavLink>
-              <NavLink to='/contact-us' activeStyle>
-                Contact Us
-              </NavLink>
-              <NavLink to='/sign-up' activeStyle>
-                Sign Up
-              </NavLink>
-              {/* Second Nav */}
-              {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
-            </NavMenu>
-            <NavBtn>
-              <NavBtnLink to='/signin'>Sign In</NavBtnLink>
-            </NavBtn>
-          </Nav>
+          <Card>
+            <header className='navbar container' >
+                <Link className='navbar__item navbar__img' to='/'><img style={{height: 50}} src={icon} /></Link>
+                <Link to='/' className='navbar__item'><h3>About Us</h3></Link>
+                <Link to='/' className='navbar__item'><h3>About Us</h3></Link>
+                <Link to='/' className='navbar__item'><h3>About Us</h3></Link>
+                
+            </header>
+          </Card>
         </>
       );
     
